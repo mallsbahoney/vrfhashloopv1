a4226728ef45ef30dd404be7e27a7681ef1ef7df
mallsbahoney <90956529+mallsbahoney@users.noreply.github.com>
2026-02-27 00:37:12 -0800

    Upload: partyserver/dist/constants.d.ts

---
+++ partyserver/dist/constants.d.ts
@@ -0,0 +1,6 @@
+export declare const ADMIN_ADDRESS = "6CcS7XFLnHA3fQLjDXUmZiN6zZhrQNScdq7AhUBkcLQZ";
+export declare const PROJECT_VAULT_ADDRESS = "Bx5FGvCat6KetUiY4nxARH1tDi7ZKH1PjU9mS6c9EvVT";
+export declare const SOL = "solana";
+export declare const USDC = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";
+export declare const POT_ID = "lottery-pot";
+export declare const MAX_NUMBER = "9007199254740992";
