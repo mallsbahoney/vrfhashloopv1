7a142d544fc62772bebe1e3d46954f198a1d27a8
mallsbahoney <90956529+mallsbahoney@users.noreply.github.com>
2026-02-27 00:37:18 -0800

    Upload: partyserver/dist/constants.js

---
+++ partyserver/dist/constants.js
@@ -0,0 +1,6 @@
+export const ADMIN_ADDRESS = "6CcS7XFLnHA3fQLjDXUmZiN6zZhrQNScdq7AhUBkcLQZ";
+export const PROJECT_VAULT_ADDRESS = "Bx5FGvCat6KetUiY4nxARH1tDi7ZKH1PjU9mS6c9EvVT";
+export const SOL = "solana";
+export const USDC = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";
+export const POT_ID = "lottery-pot";
+export const MAX_NUMBER = "9007199254740992";
